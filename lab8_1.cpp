f86adb370e55a176fb6bfeb2bab461c7396d9511
Mac <mac@Macs-MacBook-Pro.local>
2020-01-24 16:52:18 +0700

    done

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,29 +2,50 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+    //use 'setw' to set width of table and 'left' to set left-alignment
+    //you can change input argument of 'setw()' to see the effect
+    //Try to change from 'left' to 'right' and see the effect
+    double PrevBalance,Interest,Total,Payment,NewBalance;
+    cout << "Enter initial loan: ";
+    cin >> PrevBalance;
+    cout << "Enter interest rate per year (%):  ";
+    cin >> Interest;
+    Interest = Interest/100;
+    cout << "Enter amount you can pay per year: ";
+    cin >> Payment;
+
+    cout << setw(13) << left << "EndOfYear#";
+    cout << setw(13) << left << "PrevBalance";
+    cout << setw(13) << left << "Interest";
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+    
+    NewBalance = PrevBalance;
+    for(int year = 1;NewBalance>0;year++){
+
+    
+    //use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+    //you can change input argument of 'setprecision()' to see the effect
+    cout << fixed << setprecision(2);
+    cout << setw(13) << left << year;
+    cout << setw(13) << left << PrevBalance;
+    cout << setw(13) << left << Interest*PrevBalance;
+    Total = (Interest*PrevBalance)+PrevBalance;
+    cout << setw(13) << left << Total;
+    if(Payment > Total)
+    {Payment = Total;
+    }
+    cout << setw(13) << left << Payment;
+    NewBalance = Total - Payment;
+    cout << setw(13) << left << NewBalance;
+    PrevBalance = NewBalance;
+    cout << "\n";
+    }
+        return 0;
+    
 }
+
