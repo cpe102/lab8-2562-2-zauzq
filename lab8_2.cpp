bb2f3eb67fd1cbac6fd2a35e3fb4962d3a0a7ed1
Mac <mac@Macs-MacBook-Pro.local>
2020-01-24 15:59:04 +0700

    done

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 //Write the function printO() here
+int printO(int M ,int N){
+	if (M>0&&N>0)
+	{
+		for(int i=0;i < M;i++){
+			for(int j=0;j < N;j++){
+				cout << 'O';
+			}
+			cout << "\n";
+		}
+	}
+	else
+	{
+		cout << "Invalid input\n";
+	}
+	
+}
+
 
 int main(){
 	
